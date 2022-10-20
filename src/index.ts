7f0846dd0feb5993effdddb85de66dcf62d6e1f9
JoseOsorioC22 <=jossekarlos10@gmail.com>
2022-10-19 20:58:21 -0500

    suscription and unsuscription

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
-import { Observable } from "rxjs";
+import { Observable, Observer, Subscriber } from "rxjs";
 
+const observer: Observer<any> = {
+  next: value => console.log("siguiente[next]", value),
+  error: error => console.error("error", error),
+  complete: () => console.info("completado!")
+};
 
-const obs$ = new Observable<string>( suscriber =>
+const intervalo$ = new Observable<number>(suscriber =>
+{
+    //crear un contador 
+    // 1,2,3,4
+    let cont= 0; 
+
+  const interval =  setInterval( () =>
     {
-        // suscriber send values 
-            suscriber.next("hola"); 
-            suscriber.next("mundo"); 
-// suscriber complete
-            suscriber.complete();
-            
-            suscriber.next("hola"); 
-            suscriber.next("mundo"); 
-    });
+        cont = cont +1 ;
+        console.log(cont)
+        suscriber.next(cont); 
+    }, 1000); 
+
+    return () =>
+    {
+       clearInterval(interval); 
+       console.log("intervalo completado!"); 
+    }
   
+})
 
+/*
+const observer: Observer<number> = {
+  next: value =>  
+}*/
 
+const subs1 = intervalo$.subscribe( )
+const subs2 = intervalo$.subscribe( )
+const subs3 = intervalo$.subscribe( )
 
-obs$.subscribe(  resp => {console.log(resp)})
+setTimeout( () =>
+{
+    subs1.unsubscribe();
+    subs2.unsubscribe();
+    subs3.unsubscribe();
+}, 10000 )
