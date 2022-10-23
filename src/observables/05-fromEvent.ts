256c1d775fcd61a31654f94392016d62f051da47
JoseOsorioC22 <=jossekarlos10@gmail.com>
2022-10-23 14:09:32 -0500

    fromEvent - utilities

---
+++ src/observables/05-fromEvent.ts
@@ -0,0 +1,18 @@
+import { fromEvent } from "rxjs";
+
+/**
+ * eventos del DOM
+ */
+
+const eve1$ = fromEvent<MouseEvent>(document, 'click');
+const eve2$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+const obsever = {
+    next: val => console.log('next', val)
+}
+
+eve1$.subscribe(obsever);
+eve2$.subscribe((event) =>
+{
+    console.log("oprimiste una tecla: " + event.key ); 
+})
