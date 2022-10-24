a053a4b394c904c77d96061d2bfc2a757838d10e
JoseOsorioC22 <=jossekarlos10@gmail.com>
2022-10-23 19:35:49 -0500

    pluck operator

--- src/operators/map.ts
+++ src/operators/map.ts
@@ -1,5 +1,5 @@
 import { range, fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map, pluck } from 'rxjs/operators'
 
 /**
  * Map: realiza una lectura de un flujo 
@@ -19,6 +19,7 @@ range(1, 5)
 
 const keyUp$ = fromEvent<KeyboardEvent>(document, 'keyup');
 
+/*
 keyUp$
     .pipe(map<KeyboardEvent, string >( key => {
           return  key.code;  // imprimimos el keycode.
@@ -26,3 +27,27 @@ keyUp$
     .subscribe((data) => {
         console.log(data);
     }); 
+*/
+const keyUpPluck$ = keyUp$.pipe();
+
+/**
+ * Pluck: es muy util, ya que es empleado por ejemplo
+ * cuando se necesita extraer un valor de una fuente de informacion o data
+ * ejemplo: automovil = {color: red, mark: "tesla" }  => with pluck ('mark') => tesla 
+ */
+
+keyUp$
+.pipe(pluck('target', 'baseURI'))
+.subscribe( (code) => console.log('key desde fluck: ' , code)); 
+
+
+// Aunque es bueno saber para que sierve el pluck, fue removido 
+// desde la version 8 de rxJs, por lo tanto, se recomienda usar map.
+
+keyUp$
+.pipe( map<any , string >( key => 
+    {
+      return key.target.baseURI ;  //  x?.foo?.bar
+    } ) )
+.subscribe( (data) => console.log('imprimiendo la base uri con map' , data)); 
+
